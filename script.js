f8feda8225b63b0e31a7d2369fef730488052de7
Mel <brandon.melend14@gmail.com>
2022-10-01 15:41:41 -0400

    Basic functionality added

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let keyPress = "";
+let value = "";
+let operationValues = []
+let operator =""
+
 function add(num1,num2){
     return num1+num2;
 }
@@ -16,13 +20,57 @@ function divide(num1,num2){
 }
 
 function operate(op,values){
+    switch(op){
+        case "+":
+            return add(values[0],values[1]);
+        case "-":
+            return sub(values[0],values[1]);
+        case "*":
+            return multiply(values[0],values[1]);
+        case "/":
+            return divide(values[0],values[1]);
+    }
 
 }
 
 function display(e){
     const displayNum = document.querySelector(".display");
-    displayNum.textContent = e.target.textContent;
-console.log(e.target.textContent)
+    
+
+    console.log(e.target.id )
+
+    if(e.target.id !== ""){
+        if(e.target.id !== "del" && e.target.id !== "clear" && e.target.id !== "equals"){
+            operationValues.push(parseFloat(value));
+            operator = e.target.textContent;
+            console.log(operationValues.length)
+            if(operationValues.length==2){
+                value = operate(operator,operationValues);
+                operationValues = []
+                operationValues.push(parseFloat(value));
+                displayNum.textContent = value;
+                console.log(operationValues)
+            }
+
+            value = '';
+            
+        }else if(e.target.id === "equals"){  
+            operationValues.push(parseInt(value));
+            value = operate(operator,operationValues);
+            displayNum.textContent = value;
+            operationValues = [];
+            
+        }
+    }else{
+        value += e.target.textContent;
+        displayNum.textContent = value;
+
+    }
+  
+
+    // console.log('value is now: ',value )
+
+// console.log(value)
 }
 
 const keys = document.querySelectorAll('.keys');
